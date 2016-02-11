f53c4cf46bf3b7f7b6f52f8009dc2059dc785435
Dia Brown <dbrown1903@outlook.com>
2016-02-10 17:15:57 -0700

    Saving and creating algo repo (on a loaner)

---
+++ MazeSolver/MazeSolver/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MazeSolver
+{
+    /// <summary>
+    /// read a text file called maze.txt that has the adjacency list of an undirected graph.
+    /// first line of the list contains the names of all the nodes.
+    /// second line contains two nodes, indicating the start and end of the maze (in that order). 
+    /// Each additional line contains a node in the graph followed by each node to which it is adjacent. 
+    /// the nodes are comma-separated. 
+    /// may contain multiple mazes, each separated by a blank line. 
+    /// possible for: dead ends & have loops.
+    /// </summary>
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+    }
+}
