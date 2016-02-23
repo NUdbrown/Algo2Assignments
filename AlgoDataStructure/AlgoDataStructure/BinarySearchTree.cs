4bbe08d404c07932dd2876050ee2e2f979c35ca3
Dia Brown <dbrown1903@outlook.com>
2016-02-22 17:33:33 -0700

    Making it Generic

--- AlgoDataStructure/AlgoDataStructure/BinarySearchTree.cs
+++ AlgoDataStructure/AlgoDataStructure/BinarySearchTree.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace AlgoDataStructure
 {
-    public class BinarySearchTree
+    public class BinarySearchTree<T>
     {
         public void Add(T value)
         {
@@ -29,41 +29,45 @@ namespace AlgoDataStructure
         //removes the specified value from the tree if it is present, or does nothing if the value is not present.
         //If the value appears more than once, only the first occurrence of the value is removed.
         //When removing a node with two children, replace it with the next in-order value (or rather the left-most right descendant).
+        //removes all values from the tree.
         public void Clear()
         {
 
         }
 
-        //removes all values from the tree.
+        //returns the number of values in the tree 
         public void Count()
         {
 
         }
 
-        //returns the number of values in the tree 
+        //returns an in-order string representation of all the values in the BST 
         public void Inorder()
         {
 
-        }
 
-        //returns an in-order string representation of all the values in the BST 
+        }
+        
+        //returns a pre-order string representation of all the values in the BST 
         public void Preorder()
         {
 
         }
 
-        //returns a pre-order string representation of all the values in the BST 
+        /*returns a post-order string representation of all the values in the BST i. 
+        All of the “order” functions above return a string in the format of “v1, v2, …, vn”. 
+        */
         public void Postorder()
         {
 
         }
 
-        //returns a post-order string representation of all the values in the BST i.All of the “order” functions above return a string in the format of “v1, v2, …, vn”. 
+        //returns the height of the BST, where the empty tree is 0, the simple tree is 1.
         public void Height()
         {
 
         }
 
-        //returns the height of the BST, where the empty tree is 0, the simple tree is 1.
-    }
+
+     }
 }
