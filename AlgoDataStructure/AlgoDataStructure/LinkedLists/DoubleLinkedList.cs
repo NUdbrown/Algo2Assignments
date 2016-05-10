7065da6c5a857b9b67329c99a19f9bd93053d9b6
Dia Brown <dbrown1903@outlook.com>
2016-05-10 11:59:30 -0600

    fixing some stupid errors. singlelinkedlist complete

---
+++ AlgoDataStructure/AlgoDataStructure/LinkedLists/DoubleLinkedList.cs
@@ -0,0 +1,119 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AlgoDataStructure
+{
+    public class DoubleLinkedList<T> where T : IComparable<T>
+    {
+        private int _count;
+        private Node<T> _headNode;
+
+        public DoubleLinkedList()
+        {
+            Initalize();
+        } 
+        private void Initalize()
+        {
+            _count = 0;
+            _headNode = new Node<T>(default(T));
+        }
+
+        public void Add(T value)
+        {
+
+        }
+
+        public void Insert(T value, int index)
+        {
+
+        }
+
+        public int Count()
+        {
+            return _count;
+        }
+
+        public T Get(int index)
+        {
+            return default(T);
+        }
+
+        public T Remove()
+        {
+            return default(T);
+        }
+
+        public T RemoveAt()
+        {
+            return default(T);
+        }
+
+        public T RemoveLast()
+        {
+            return default(T);
+        }
+
+        public void Clear()
+        {
+            Initalize();
+        }
+
+        public int Search(T value)
+        {
+            return 0;
+        }
+        
+        public string ToString()
+        {
+            return null;
+        }
+
+        protected class Node<T> where T : IComparable<T>
+        {
+            private T _data;
+            private Node<T> _isNextNode;
+            private Node<T> _isPreviousNode; 
+
+            public Node(T initalData)
+            {
+                _data = initalData;
+                _isNextNode = null;
+            }
+
+            public T GetData()
+            {
+                return _data;
+            }
+
+            public void SetData(T data)
+            {
+                _data = data;
+            }
+
+            public Node<T> GetIsNextNode()
+            {
+                return _isNextNode;
+            }
+
+            public void SetIsNextNode(Node<T> next)
+            {
+                _isNextNode = next;
+            }
+
+            public Node<T> GetIsPreviousNode()
+            {
+                return _isPreviousNode;
+            }
+
+            public void SetIsPreviousNode(Node<T> previous)
+            {
+                _isPreviousNode = previous;
+            }
+        }
+
+    }
+
+}
---
+++ AlgoDataStructure/AlgoDataStructure/LinkedLists/SingleLinkedList.cs
@@ -0,0 +1,216 @@
+﻿using System;
+
+namespace AlgoDataStructure
+{
+    public class SingleLinkedList<T> where T : IComparable<T>
+    {
+
+        private int _count;
+        private Node<T> _headNode; 
+
+        public SingleLinkedList()
+        {
+            Initialize();
+        }
+     
+        private void Initialize()
+        {
+            _count = 0;
+            _headNode = new Node<T>(default(T));
+            
+        }
+
+        public void Add(T value)
+        {
+            Node<T> temp = new Node<T>(value);
+            Node<T> current = _headNode;
+            
+            while (current.GetIsNextNode() != null)
+            {
+                current = current.GetIsNextNode();
+            }
+
+            current.SetIsNextNode(temp);
+            _count++;
+        }
+
+        public void Insert(T value, int index)
+        {
+            Node<T> temp = new Node<T>(value);
+            Node<T> current = _headNode;
+
+            for (int i = 1; i < index && current.GetIsNextNode() != null; i++)
+            {
+                current = current.GetIsNextNode();
+            }
+
+            temp.SetIsNextNode(current.GetIsNextNode());
+            current.SetIsNextNode(temp);
+            _count++;
+        }
+
+        public int Count()
+        {
+            return _count;
+        }
+
+        public T Get(int index)
+        {
+            if (index <= 0)
+                return default(T);
+
+            Node<T> current = _headNode.GetIsNextNode();
+            for (int i = 1; i < index; i++)
+            {
+                if (current.GetIsNextNode() == null)
+                    return default(T);
+
+                current = current.GetIsNextNode();
+            }
+            return current.GetData();
+        }
+
+        public T Remove()
+        {
+            Node<T> current = _headNode;
+
+            _headNode = current.GetIsNextNode();
+
+
+            current.SetIsNextNode(current.GetIsNextNode().GetIsNextNode());
+            _count--;
+
+
+            return _headNode.GetData();
+        }
+
+        public T RemoveAt(int index)
+        {
+            T val = Get(index);
+
+            if (index > 1 && index < Count())
+            {
+                Node<T> current = _headNode;
+
+                for (int i = 1; i < index; i++)
+                {
+                    if (current.GetIsNextNode() != null)
+                    {
+                        current = current.GetIsNextNode();
+                    }
+
+                }
+
+                current.SetIsNextNode(current.GetIsNextNode().GetIsNextNode());
+
+            }
+
+            _count--;
+
+            return val;
+        }
+
+        public T RemoveLast()
+        {
+            Node<T> tail = new Node<T>(Get(Count()));
+            Node<T> current = _headNode;
+            Node<T> previous = null;
+
+            T last = Get(Count());
+
+
+            while (current.GetIsNextNode() != null)
+            {
+                previous = current;
+                current = current.GetIsNextNode();
+            }
+
+            Node<T> result = tail;
+            tail = previous;
+            if (tail == null)
+                _headNode = null;
+            else
+                tail.SetIsNextNode(null);
+            _count--;
+
+
+            return last;
+        }
+
+        public string ToString()
+        {
+            Node<T> current = _headNode.GetIsNextNode();
+            string _out = "";
+
+            while (current != null)
+            {
+                _out += "[" + current.GetData() + "]";
+                current = current.GetIsNextNode();
+            }
+
+            return _out;
+        }
+
+        //removes all values in the list
+        public void Clear()
+        {
+            Initialize();
+
+        }
+
+       public int Search(T value)
+        {
+
+            Node<T> current = _headNode;
+
+            int index = -1;
+
+            for (int i = 1; i < Count() && current.GetIsNextNode() != null; i++)
+            {
+                if (current.GetData().Equals(value))
+                {
+                    return i;
+                }
+
+            }
+
+            return index;
+
+        }
+
+        protected class Node<T> where T : IComparable<T>
+        {
+            private T _data;
+            private Node<T> _isNextNode;
+        
+            public Node(T initalData)
+            {
+                _data = initalData;
+                _isNextNode = null;
+            }
+
+            public T GetData()
+            {
+                return _data;
+            }
+
+            public void SetData(T data)
+            {
+                _data = data;
+            }
+
+            public Node<T> GetIsNextNode()
+            {
+                return _isNextNode;
+            }
+
+            public void SetIsNextNode(Node<T> next)
+            {
+                _isNextNode = next;
+            }
+
+
+
+        }
+    }
+}
